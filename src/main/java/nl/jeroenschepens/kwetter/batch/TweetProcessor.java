79d525b8b906533af423682f44235de310d28852
Jeroen <jeroenschepens@live.nl>
2015-03-30 20:14:42 +0200

    Finished tweet batch import job

--- src/main/java/nl/jeroenschepens/kwetter/batch/TweetProcessor.java
+++ src/main/java/nl/jeroenschepens/kwetter/batch/TweetProcessor.java
@@ -1,15 +1,30 @@
 package nl.jeroenschepens.kwetter.batch;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.batch.api.chunk.ItemProcessor;
 import javax.inject.Named;
+
+import nl.jeroenschepens.kwetter.domain.Tweet;
 
 @Named
 public class TweetProcessor implements ItemProcessor {
 
+	private static final DateFormat DATE_FORMAT = new SimpleDateFormat(
+			"yyyy-MM-dd HH:mm:ss");
+
 	@Override
 	public Object processItem(Object input) throws Exception {
-		// TODO write implementation
-		System.out.println(input);
-		return input;
+		InputTweet inputTweet = (InputTweet) input;
+		Date postDate = DATE_FORMAT.parse(inputTweet.getPostDate());
+		String poster = inputTweet.getScreenName().toLowerCase();
+		Tweet tweet = new Tweet();
+		tweet.setTweet(inputTweet.getTweet());
+		tweet.setPoster(poster);
+		tweet.setPostdate(postDate);
+		tweet.setPostedfrom(inputTweet.getPostedFrom());
+		return tweet;
 	}
 }
--- src/main/java/nl/jeroenschepens/kwetter/batch/TweetWriter.java
+++ src/main/java/nl/jeroenschepens/kwetter/batch/TweetWriter.java
@@ -3,14 +3,25 @@ package nl.jeroenschepens.kwetter.batch;
 import java.util.List;
 
 import javax.batch.api.chunk.AbstractItemWriter;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+
+import nl.jeroenschepens.kwetter.domain.Tweet;
 
 @Named
 public class TweetWriter extends AbstractItemWriter {
 
+	@Inject
+	private EntityManager em;
+
 	@Override
 	public void writeItems(List<Object> items) throws Exception {
-		// TODO write implementation
-		System.out.println("Write " + items.size() + " items");
+		@SuppressWarnings("unchecked")
+		List<Tweet> tweets = (List<Tweet>) (List<?>) items;
+		for (Tweet tweet : tweets) {
+			System.out.println(tweet);
+			em.persist(tweet);
+		}
 	}
 }
